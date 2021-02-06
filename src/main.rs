8343a20f357bb4707a3d1c1698064babae2d7277
General Host <generalhost@criptext.com>
2021-02-06 19:36:55 +0300

    noe snake can fully turn and there's no residual tail

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 
 use pancurses::*;
 use rand::{thread_rng, Rng};
-use std::thread;
-use std::time::Duration;
 
 static DEFAULT_PART_CHAR: char = '#';
 static DEFAULT_SNAKE_SIZE: usize = 10;
@@ -27,7 +25,6 @@ fn main() {
     init_pair(5, COLOR_WHITE, COLOR_BLACK);
     init_pair(6, COLOR_BLUE, COLOR_BLACK);
     init_pair(7, COLOR_GREEN, COLOR_BLACK);
-    // init_pair(8, COLOR_BLACK  , COLOR_BLACK);
 
     curs_set(0);
     noecho();
@@ -42,6 +39,7 @@ struct Snake<'a> {
     dir: SnakeDir,
     window: &'a Window,
     parts: Vec<SnakePart>,
+    length: usize,
 }
 
 impl<'a> Snake<'a> {
@@ -59,24 +57,44 @@ impl<'a> Snake<'a> {
             form,
             parts,
             window,
+            length: size,
         }
+    }
+
+    fn sync(&mut self) {
+        let mut parts = self.parts.clone();
+        parts.reverse();
+        self.parts.clear();
+
+        for i in 0..self.length {
+            self.parts.push(SnakePart::new(
+                parts[i].y,
+                parts[i].x,
+            ))
+        }
+        self.parts.reverse();
     }
 
     fn start(&mut self) {
         while !self.check_game_over() {
+            self.sync();
             half_delay(1);
             self.dir = match self.window.getch() {
                 Some(Input::Character('w')) => SnakeDir::Up,
                 Some(Input::Character('s')) => SnakeDir::Down,
                 Some(Input::Character('a')) => SnakeDir::Left,
                 Some(Input::Character('d')) => SnakeDir::Right,
+                Some(Input::KeyUp) => SnakeDir::Up,
+                Some(Input::KeyDown) => SnakeDir::Down,
+                Some(Input::KeyLeft) => SnakeDir::Left,
+                Some(Input::KeyRight) => SnakeDir::Right,
                 _ => self.dir,
             };
 
             self.mv();
 
             for part in &self.parts {
-                let color = rand_color();
+                let color = snake_color();
 
                 self.window.attron(COLOR_PAIR(color));
                 self.window.mvaddch(part.y, part.x, self.form);
@@ -84,92 +102,26 @@ impl<'a> Snake<'a> {
 
                 self.window.refresh();
             }
-
-            // thread::sleep(Duration::from_millis(100));
-            // self.window.clear();
         }
     }
 
     fn mv(&mut self) {
         let mut last_part = self.last_part();
         match self.dir {
-            SnakeDir::Up => {
-                last_part.mv(last_part.y - 1, last_part.x);
-                for part in &mut self.parts_without_last() {
-                    match part.dir {
-                        SnakeDir::Up => {
-                            part.mv(part.y - 1, part.x);
-                        }
-                        SnakeDir::Down => {
-                            part.mv(part.y + 1, part.x);
-                        }
-                        SnakeDir::Left => {
-                            part.mv(part.y, part.x - 1);
-                        }
-                        SnakeDir::Right => {
-                            part.mv(part.y, part.x + 1);
-                        }
-                    }
-                }
-            }
-            SnakeDir::Down => {
-                last_part.mv(last_part.y + 1, last_part.x);
-                for part in &mut self.parts_without_last() {
-                    match part.dir {
-                        SnakeDir::Up => {
-                            part.mv(part.y - 1, part.x);
-                        }
-                        SnakeDir::Down => {
-                            part.mv(part.y + 1, part.x);
-                        }
-                        SnakeDir::Left => {
-                            part.mv(part.y, part.x - 1);
-                        }
-                        SnakeDir::Right => {
-                            part.mv(part.y, part.x + 1);
-                        }
-                    }
-                }
-            }
-            SnakeDir::Left => {
-                last_part.mv(last_part.y, last_part.x - 1);
-                for part in &mut self.parts_without_last() {
-                    match part.dir {
-                        SnakeDir::Up => {
-                            part.mv(part.y - 1, part.x);
-                        }
-                        SnakeDir::Down => {
-                            part.mv(part.y + 1, part.x);
-                        }
-                        SnakeDir::Left => {
-                            part.mv(part.y, part.x - 1);
-                        }
-                        SnakeDir::Right => {
-                            part.mv(part.y, part.x + 1);
-                        }
-                    }
-                }
-            }
-            SnakeDir::Right => {
-                last_part.mv(last_part.y, last_part.x + 1);
-                for part in &mut self.parts_without_last() {
-                    match part.dir {
-                        SnakeDir::Up => {
-                            part.mv(part.y - 1, part.x);
-                        }
-                        SnakeDir::Down => {
-                            part.mv(part.y + 1, part.x);
-                        }
-                        SnakeDir::Left => {
-                            part.mv(part.y, part.x - 1);
-                        }
-                        SnakeDir::Right => {
-                            part.mv(part.y, part.x + 1);
-                        }
-                    }
-                }
+            SnakeDir::Up => last_part.mv(last_part.y - 1, last_part.x),
+            SnakeDir::Down => last_part.mv(last_part.y + 1, last_part.x),
+            SnakeDir::Left => last_part.mv(last_part.y, last_part.x - 1),
+            SnakeDir::Right => last_part.mv(last_part.y, last_part.x + 1),
+        }
+        for part in &mut self.parts_without_last() {
+            match part.dir {
+                SnakeDir::Up => part.mv(part.y - 1, part.x),
+                SnakeDir::Down => part.mv(part.y + 1, part.x),
+                SnakeDir::Left => part.mv(part.y, part.x - 1),
+                SnakeDir::Right => part.mv(part.y, part.x + 1),
             }
         }
+        self.window.clear();
         self.parts.push(last_part);
     }
 
@@ -177,12 +129,8 @@ impl<'a> Snake<'a> {
         false
     }
 
-    fn len(&self) -> usize {
-        self.parts.len()
-    }
-
     fn last_part(&self) -> SnakePart {
-        self.parts[self.len() - 1]
+        self.parts[self.parts.len() - 1]
     }
 
     fn parts_without_last(&self) -> Vec<SnakePart> {
@@ -224,7 +172,7 @@ impl SnakePart {
     }
 }
 
-fn rand_color() -> u32 {
+fn snake_color() -> u32 {
     let mut rng = thread_rng();
     rng.gen_range(1..=7)
 }
