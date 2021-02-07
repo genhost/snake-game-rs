66ea2bd230e6ca9a3df11c91a109df9d8e4221de
General Host <generalhost@criptext.com>
2021-02-07 13:52:59 +0300

    Now there's a --help command to get help about snake_game arguments. Check it out

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,173 @@
 #![allow(dead_code)]
 
+use clap::{App, Arg};
 use pancurses::*;
 use rand::{thread_rng, Rng};
+use std::convert::TryInto;
 
 static DEFAULT_SNAKE_DIR: SnakeDir = SnakeDir::Right;
-static DEFAULT_APPLE_INCSIZE: usize = 5;
+static DEFAULT_SNAKE_COLOR: i16 = COLOR_PAIR_GREEN;
 static DEFAULT_SNAKE_SIZE: usize = 10;
-static DEFAULT_APPLE_CHAR: char = '#';
 static DEFAULT_PART_CHAR: char = '#';
+static DEFAULT_APPLE_CHAR: char = '#';
+static DEFAULT_APPLE_COLOR: i16 = COLOR_PAIR_RED;
+static DEFAULT_APPLE_INCSIZE: usize = 5;
+static DEFAULT_DISPLAY_STATE: &str = "points";
+
+// Don't change these constants
+static COLOR_PAIR_WHITE: i16 = 1;
+static COLOR_PAIR_BLUE: i16 = 2;
+static COLOR_PAIR_CYAN: i16 = 3;
+static COLOR_PAIR_GREEN: i16 = 4;
+static COLOR_PAIR_RED: i16 = 5;
+static COLOR_PAIR_YELLOW: i16 = 6;
+static COLOR_PAIR_MAGENTA: i16 = 7;
+static COLOR_PAIR_BLACK: i16 = 8;
+static COLOR_RANDOM: i16 = 9;
 
 fn main() {
+    let matches = App::new("Snake Game")
+        .author("General Host")
+        .about("Simple snake game written in Rust using pancurses")
+        .arg(
+            Arg::with_name("snake char")
+                .long("snake-char")
+                .value_name("CHAR")
+                .help(&format!("Sets snake char. Default is {}", DEFAULT_PART_CHAR)),
+        )
+        .arg(
+            Arg::with_name("snake size")
+                .long("snake-size")
+                .value_name("NUMBER")
+                .help(&format!("Sets initial snake size. Default is {}", DEFAULT_SNAKE_SIZE)),
+        )
+        .arg(
+            Arg::with_name("snake direction")
+                .long("snake-dir")
+                .value_name("up | down | left | right")
+                .help("Sets initial snake direction. Default is right"),
+        )
+        .arg(
+            Arg::with_name("snake color")
+                .long("snake-color")
+                .value_name("red | white | magenta | cyan | black | green | blue | yellow")
+                .help("Sets initial snake color. Default is green"),
+        )
+        .arg(
+            Arg::with_name("apple increase size")
+                .long("apple-inc")
+                .value_name("NUMBER")
+                .help(&format!("Sets how much the snake will increase in size after it eats an apple. Default is {}", DEFAULT_APPLE_INCSIZE)),
+        )
+        .arg(
+            Arg::with_name("apple char")
+                .long("apple-char")
+                .value_name("CHAR")
+                .help(&format!("Sets apple char. Default is {}", DEFAULT_APPLE_CHAR)),
+        )
+        .arg(
+            Arg::with_name("apple color")
+                .long("apple-color")
+                .value_name("red | white | magenta | cyan | black | green | blue | yellow | random")
+                .help("Sets apple color. Default is red"),
+        )
+        .arg(
+            Arg::with_name("hardcore")
+                .short("h")
+                .multiple(true)
+                .help("If you are bored with the usual snake, this flag is for you. The more you add this flag (-hh, -hhh etc), the more difficult it will be to play, to the point of being unplayable. Note: Game with this flag is being very unstable"),
+        )
+        .arg(
+            Arg::with_name("display")
+                .long("display")
+                .value_name("none | points")
+                .help("Change what to display at the game time. Default is points"),
+        )
+        .get_matches();
+
+    let snake_char = matches
+        .value_of("snake char")
+        .unwrap_or(&String::from(DEFAULT_PART_CHAR))
+        .chars()
+        .nth(0)
+        .unwrap();
+    let snake_size = matches
+        .value_of("snake size")
+        .unwrap_or(&DEFAULT_SNAKE_SIZE.to_string())
+        .parse::<usize>()
+        .unwrap();
+    let snake_dir = match matches.value_of("snake direction") {
+        Some("up") => SnakeDir::Up,
+        Some("down") => SnakeDir::Down,
+        Some("left") => SnakeDir::Left,
+        Some("right") => SnakeDir::Right,
+        _ => DEFAULT_SNAKE_DIR,
+    };
+    let snake_color = match matches.value_of("snake color") {
+        Some("white") => COLOR_PAIR_WHITE,
+        Some("blue") => COLOR_PAIR_BLUE,
+        Some("cyan") => COLOR_PAIR_CYAN,
+        Some("black") => COLOR_PAIR_BLACK,
+        Some("red") => COLOR_PAIR_RED,
+        Some("green") => COLOR_PAIR_GREEN,
+        Some("yellow") => COLOR_PAIR_YELLOW,
+        Some("magenta") => COLOR_PAIR_MAGENTA,
+        Some("random") => COLOR_RANDOM,
+        _ => DEFAULT_SNAKE_COLOR,
+    };
+    let apple_char = matches
+        .value_of("apple char")
+        .unwrap_or(&String::from(DEFAULT_APPLE_CHAR))
+        .chars()
+        .nth(0)
+        .unwrap();
+    let apple_incsize = matches
+        .value_of("apple increase size")
+        .unwrap_or(&DEFAULT_APPLE_INCSIZE.to_string())
+        .parse::<usize>()
+        .unwrap();
+    let apple_color = match matches.value_of("apple color") {
+        Some("white") => COLOR_PAIR_WHITE,
+        Some("blue") => COLOR_PAIR_BLUE,
+        Some("cyan") => COLOR_PAIR_CYAN,
+        Some("black") => COLOR_PAIR_BLACK,
+        Some("red") => COLOR_PAIR_RED,
+        Some("green") => COLOR_PAIR_GREEN,
+        Some("yellow") => COLOR_PAIR_YELLOW,
+        Some("magenta") => COLOR_PAIR_MAGENTA,
+        Some("random") => COLOR_RANDOM,
+        _ => DEFAULT_APPLE_COLOR,
+    };
+    let hardcore_level = matches.occurrences_of("hardcore") as i32;
+    let display = matches.value_of("display").unwrap_or(DEFAULT_DISPLAY_STATE);
+
     let window = initscr();
-    let mut snake = Snake::new(
-        &window,
-        DEFAULT_PART_CHAR,
-        DEFAULT_SNAKE_SIZE,
-        DEFAULT_APPLE_INCSIZE,
-        DEFAULT_SNAKE_DIR,
-        Apple::new(&window, DEFAULT_APPLE_CHAR, 3),
-    );
 
     start_color();
 
-    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
-    init_pair(2, COLOR_CYAN, COLOR_BLACK);
-    init_pair(3, COLOR_RED, COLOR_BLACK);
-    init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
-    init_pair(5, COLOR_WHITE, COLOR_BLACK);
-    init_pair(6, COLOR_BLUE, COLOR_BLACK);
-    init_pair(7, COLOR_GREEN, COLOR_BLACK);
+    init_pair(COLOR_PAIR_YELLOW, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(COLOR_PAIR_CYAN, COLOR_CYAN, COLOR_BLACK);
+    init_pair(COLOR_PAIR_RED, COLOR_RED, COLOR_BLACK);
+    init_pair(COLOR_PAIR_MAGENTA, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(COLOR_PAIR_WHITE, COLOR_WHITE, COLOR_BLACK);
+    init_pair(COLOR_PAIR_BLUE, COLOR_BLUE, COLOR_BLACK);
+    init_pair(COLOR_PAIR_GREEN, COLOR_GREEN, COLOR_BLACK);
+    init_pair(COLOR_PAIR_BLACK, COLOR_BLACK, COLOR_BLACK);
 
     curs_set(0);
     noecho();
+
+    let mut snake = Snake::new(
+        &window,
+        display,
+        snake_char,
+        snake_size,
+        apple_incsize,
+        snake_dir,
+        snake_color.try_into().unwrap(),
+        Apple::new(&window, apple_char, apple_color),
+        hardcore_level,
+    );
 
     snake.start();
 
@@ -40,22 +176,29 @@ fn main() {
 
 struct Snake<'a> {
     form: char,
+    color: u32,
     dir: SnakeDir,
+    length: usize,
+    hardcore: i32,
+    points: usize,
+    inc_size: usize,
+    display: &'a str,
+    apple: Apple<'a>,
     window: &'a Window,
     parts: Vec<SnakePart>,
-    length: usize,
-    apple: Apple<'a>,
-    inc_size: usize,
 }
 
 impl<'a> Snake<'a> {
     fn new(
         window: &'a Window,
+        display: &'a str,
         form: char,
         size: usize,
         inc_size: usize,
         dir: SnakeDir,
+        color: u32,
         apple: Apple<'a>,
+        hardcore: i32,
     ) -> Self {
         let mut parts: Vec<SnakePart> = Vec::new();
         let window_center = (window.get_max_y() / 2, window.get_max_x() / 2);
@@ -65,13 +208,20 @@ impl<'a> Snake<'a> {
                 window_center.1 + x_offset as i32,
             ))
         }
+        if let SnakeDir::Left = dir {
+            parts.reverse()
+        }
         Self {
             dir,
             form,
             parts,
             window,
             apple,
+            display,
+            points: 0,
+            color,
             inc_size,
+            hardcore,
             length: size,
         }
     }
@@ -88,7 +238,7 @@ impl<'a> Snake<'a> {
     }
 
     fn start(&mut self) {
-        loop {
+        while !self.check_game_over() {
             self.sync();
             half_delay(1);
             self.dir = match self.window.getch() {
@@ -103,24 +253,24 @@ impl<'a> Snake<'a> {
                 _ => self.dir,
             };
 
-            
             self.check_eaten();
             self.mv();
             self.redraw();
-            
-            if self.check_game_over() {
-                break;
-            }
-
         }
     }
 
     fn last_part_mv(&mut self, last_part: &mut SnakePart) {
+        let offset;
+        if self.hardcore > 0 {
+            offset = self.hardcore;
+        } else {
+            offset = 1;
+        }
         match self.dir {
-            SnakeDir::Up => last_part.mv(last_part.y - 1, last_part.x),
-            SnakeDir::Down => last_part.mv(last_part.y + 1, last_part.x),
-            SnakeDir::Left => last_part.mv(last_part.y, last_part.x - 1),
-            SnakeDir::Right => last_part.mv(last_part.y, last_part.x + 1),
+            SnakeDir::Up => last_part.mv(last_part.y - offset, last_part.x),
+            SnakeDir::Down => last_part.mv(last_part.y + offset, last_part.x),
+            SnakeDir::Left => last_part.mv(last_part.y, last_part.x - offset),
+            SnakeDir::Right => last_part.mv(last_part.y, last_part.x + offset),
         }
     }
 
@@ -139,10 +289,10 @@ impl<'a> Snake<'a> {
     }
 
     fn check_game_over(&mut self) -> bool {
-        if self.last_part().y == self.window.get_beg_y() - 1
-            || self.last_part().x == self.window.get_beg_x() - 1
-            || self.last_part().y == self.window.get_max_y()
-            || self.last_part().x == self.window.get_max_x()
+        if self.last_part().y <= self.window.get_beg_y() - 1
+            || self.last_part().x <= self.window.get_beg_x() - 1
+            || self.last_part().y >= self.window.get_max_y()
+            || self.last_part().x >= self.window.get_max_x()
         {
             return true;
         } else {
@@ -166,22 +316,22 @@ impl<'a> Snake<'a> {
     }
 
     fn redraw(&self) {
+        // std::thread::sleep(std::time::Duration::from_millis(0));
         self.window.clear();
         self.apple.spawn();
 
-        self.window.mvaddstr(
-            self.window.get_beg_y(),
-            self.window.get_beg_x(),
-            format!(
-                "Parts Vector: {} | Length: {}",
-                self.parts.len(),
-                self.length
-            )
-            .as_str(),
-        );
+        if let "points" = self.display {
+            self.window.mvaddstr(self.window.get_beg_y(),
+                self.window.get_beg_x(),
+                &format!("Points: {}\nLength: {}", self.points, self.length)
+            );
+        }
 
         for part in &self.parts {
-            let color = snake_color();
+            let color = match self.color {
+                9 => rand_color(),
+                _ => self.color,
+            };
 
             self.window.attron(COLOR_PAIR(color));
             self.window.mvaddch(part.y, part.x, self.form);
@@ -195,6 +345,7 @@ impl<'a> Snake<'a> {
         if self.last_part().y == self.apple.y && self.last_part().x == self.apple.x {
             self.apple.rand_move();
             self.inc_size(self.inc_size);
+            self.points += 1;
         }
     }
 
@@ -246,7 +397,7 @@ impl SnakePart {
     }
 }
 
-fn snake_color() -> u32 {
+fn rand_color() -> u32 {
     thread_rng().gen_range(1..=7)
 }
 
@@ -254,15 +405,15 @@ struct Apple<'a> {
     x: i32,
     y: i32,
     form: char,
-    color: u32,
+    color: i16,
     window: &'a Window,
 }
 
 impl<'a> Apple<'a> {
-    fn new(window: &'a Window, form: char, color: u32) -> Self {
+    fn new(window: &'a Window, form: char, color: i16) -> Self {
         Self {
-            x: thread_rng().gen_range(window.get_beg_x()..window.get_max_x()),
-            y: thread_rng().gen_range(window.get_beg_y()..window.get_max_y()),
+            x: thread_rng().gen_range(window.get_beg_x()..=window.get_max_x()),
+            y: thread_rng().gen_range(window.get_beg_y()..=window.get_max_y()),
             form,
             color,
             window,
@@ -270,13 +421,15 @@ impl<'a> Apple<'a> {
     }
 
     fn rand_move(&mut self) {
-        self.x = thread_rng().gen_range(self.window.get_beg_x()..self.window.get_max_x());
-        self.y = thread_rng().gen_range(self.window.get_beg_y()..self.window.get_max_y());
+        self.x = thread_rng().gen_range(self.window.get_beg_x()..=self.window.get_max_x());
+        self.y = thread_rng().gen_range(self.window.get_beg_y()..=self.window.get_max_y());
     }
 
     fn spawn(&self) {
-        self.window.attron(COLOR_PAIR(self.color));
+        self.window
+            .attron(COLOR_PAIR(self.color.try_into().unwrap()));
         self.window.mvaddch(self.y, self.x, self.form);
-        self.window.attroff(COLOR_PAIR(self.color));
+        self.window
+            .attroff(COLOR_PAIR(self.color.try_into().unwrap()));
     }
 }
